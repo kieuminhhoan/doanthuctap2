705708ae9556209e275f71e64d0e4c9ad7289d22
kieuminhhoan <hoankieupisces@gmail.com>
2019-09-25 17:17:30 +0700

    1

---
+++ index.php
@@ -0,0 +1,92 @@
+<!<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+
+</head>
+<body>
+<div class="warpper">
+    <div class="header">
+        <div class="header-top">
+            <div class="clear-fix">
+                <div class="col col-50">
+                    <div class="header-left">
+                        <span>Hotline: 0374196777</span>
+                    </div>
+                </div>
+                <div class="col col-50">
+                    <div class="header-right">
+                        <ul>
+                            <li><a href="#"><i class="fas fa-list-ol"> Kiểm tra đơn hàng</i></a></li>
+                            <li><a href="#"><i class="fas fa-shopping-cart"> Giỏ hàng</i></a></li>
+                            <li><a href="#"><i class="fas fa-sign-in-alt"> Đăng nhập</i></a></li>
+                            <li><a href="#"><i class="fas fa-user-plus"> Đăng ký</i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="main-header">
+            <div class="clear-fix">
+                <div class="col col-50">
+                    <div class="image">
+                        <img src="img/logo.png">
+                    </div>
+                </div>
+                <div class="col col-50">
+                    <div class="search">
+                            <input type="text" name="" placeholder="Search..." />
+                        <button><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li class="page-main"><a href="#">TRANG CHỦ</a></li>
+                <li><a href="#">SẢN PHẨM</a></li>
+                <li><a href="#">TIN TỨC</a></li>
+                <li><a href="#">GIỚI THIỆU</a></li>
+                <li><a href="#">LIÊN HỆ</a></li>
+            </ul>
+        </div>
+    <div class="content">
+        <div class="clear-fix">
+            <div class="col col-30">
+                <div class="menu-doc">
+                    <ul>
+                        <li><a href="#">ÁO</a>
+                            <ul class="sub-menu">
+                                <li><a href="#">ÁO NAM</a></li>
+                                <li><a href="#">ÁO NỮ</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">QUẦN</a>
+                            <ul class="sub-menu">
+                                <li><a href="#">QUẦN ÂU</a></li>
+                                <li><a href="#">QUẦN JEAN</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">VÁY</a></li>
+                        <li><a href="#">PHỤ KIỆN</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col col-70">
+                <div class="banner">
+                    <img src="img/banner1.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
